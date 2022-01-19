a19ba8da1314ba1264180475e4d2708fd38139e3
Jorge Nunes <jlbnunes@live.com>
2022-01-19 11:06:46 -0300

    commit initial

---
+++ Back/database/migrations/2022_01_19_134454_create_prices_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePricesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prices', function (Blueprint $table) {
+            $table->id();
+            $table->integer('codigo_id')->unsigned();
+            $table->integer('minimo_vidas');
+            $table->double('faixa1',10,2);
+            $table->double('faixa2',10,2);
+            $table->double('faixa3',10,2);
+            $table->foreign('codigo_id')->references('id')->on('plans')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prices');
+    }
+}
